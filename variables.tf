48941c70f338d020fedc4d6d7944af561b2771a7
pranavkumarpk01 <124168954+pranavkumarpk01@users.noreply.github.com>
2024-02-26 11:58:12 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+}
