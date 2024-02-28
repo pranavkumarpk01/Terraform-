1ce21b18eba5723244bbcf023c6327dc0ca72ac1
pranavkumarpk01 <124168954+pranavkumarpk01@users.noreply.github.com>
2024-02-28 12:29:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,12 +16,12 @@ provider "azurerm" {
 }
 #creating a resource group
 resource "azurerm_resource_group" "resource_group_test" {
-    name = "rg-terraform-created"
+    name = "rg-terraform-terra"
     location = "South India"
   }
 #creating a storage acccount
 resource "azurerm_storage_account" "storage_account" {
-  name                     ="tstorage21250107"
+  name                     ="tstorage2125"
   resource_group_name      = azurerm_resource_group.resource_group_test.name
   location                 = azurerm_resource_group.resource_group_test.location
   account_tier             = "Standard"
